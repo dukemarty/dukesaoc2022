77313f8c3a2d7c604154c612215d984d60fcbbb2
Martin Loesch <martin@martinloesch.net>
2023-01-03 01:15:47 +0100

    Solved day 16, part 1 in TypeScript.

--- DenoTypescript/day16/day16.ts
+++ DenoTypescript/day16/day16.ts
@@ -109,33 +109,83 @@ class Network {
         this.graph.calcShortestPaths();
     }
 
-    simplifyOLD(removals: Array<string>) {
-        removals.forEach(r => {
-            this.graph.flattenOutNode(this.nameToIndex.get(r)!);
-        });
-    }
-
     searchMaxRelease(initialValveRates: Map<string, number>, time: number) {
         let maxRelease = 0;
         const states = new Array<SearchState>();
+        const processed = new Array<SearchState>();
 
         const initialState = new SearchState("AA", this.graph.shortAdjMatrix, initialValveRates, 0, 0);
         states.push(initialState);
 
-        while (states.length > 0) {
+        let noImprovementOfMaxReleaseCount = 0;
+        while (states.length > 0) { // && noImprovementOfMaxReleaseCount < 100000) {
+            states.sort((a, b) => b.heuristic - a.heuristic);
+            // states.forEach(s => console.log("Heur: ", s.heuristic));
+            // console.log("------------------");
             const nextState = states.shift()!;
+            // console.log(`eval. ${nextState.accumulatedRelease}/${nextState.heuristic} vs. best: ${maxRelease}`);
+            // if ((nextState.accumulatedRelease + 10 > maxRelease) && (nextState.heuristic + 10 < maxRelease)) {
+            if (nextState.heuristic + 10 < maxRelease) {
+                console.log("Found stop condition:");
+                console.log(`    eval. ${nextState.accumulatedRelease}/${nextState.heuristic} vs. best: ${maxRelease}`);
+                continue;
+            }
             // console.log(`Evaluating in ${nextState.pos} at t=${nextState.time}: ${nextState.accumulatedRelease}`);
             // console.log(nextState);
-            if (states.length % 100 == 0) { console.log(states.length); }
-            if (nextState.time >= 29 || Array.from(nextState.valveRates.values()).reduce((acc, curr) => acc + curr, 0) == 0) {
+            if (states.length % 20 == 0) {
+                // console.log(states.length);
+                console.log(`  eval. ${nextState.accumulatedRelease}/${nextState.heuristic} vs. best: ${maxRelease}`);
+            }
+            if (nextState.time >= time || Math.max(...nextState.valveRates.values()) == 0) {
                 if (nextState.accumulatedRelease > maxRelease) {
                     maxRelease = nextState!.accumulatedRelease;
                     console.log("New max release: ", maxRelease);
+                    noImprovementOfMaxReleaseCount = 0;
+                } else {
+                    ++noImprovementOfMaxReleaseCount;
                 }
                 continue;
             }
 
-            states.push(...this.expand(nextState));
+            const expansion = this.expand(nextState);
+            const newStates = new Array<SearchState>();
+            for (let i = 0; i < expansion.length; ++i) {
+                const stateToCheck = expansion[i];
+                const rateNamesToCheck = Array.from(stateToCheck.valveRates.keys());
+                const eqState = states.find((s, idx) => {
+                    const eqPos = stateToCheck.pos == s.pos;
+                    const eqTime = stateToCheck.time == s.time;
+                    const eqRates = rateNamesToCheck.map(name => stateToCheck.valveRates.get(name) == s.valveRates.get(name)).reduce((acc, curr) => acc && curr, true);
+                    return eqPos && eqTime && eqRates;
+                });
+                const procEqState = processed.find((s, idx) => {
+                    const eqPos = stateToCheck.pos == s.pos;
+                    const eqTime = stateToCheck.time == s.time;
+                    const eqRates = rateNamesToCheck.map(name => stateToCheck.valveRates.get(name) == s.valveRates.get(name)).reduce((acc, curr) => acc && curr, true);
+                    return eqPos && eqTime && eqRates;
+                });
+                if (eqState == undefined) {
+                    if (procEqState == undefined) {
+                        newStates.push(stateToCheck);
+                    } else {
+                        if (stateToCheck.accumulatedRelease > procEqState.accumulatedRelease) {
+                            newStates.push(stateToCheck);
+                        } else {
+                            console.log("Already processed better version of state");
+                        }
+                    }
+                } else {
+                    if (stateToCheck.accumulatedRelease > eqState.accumulatedRelease) {
+                        eqState.accumulatedRelease = stateToCheck.accumulatedRelease;
+                        eqState.heuristic = stateToCheck.heuristic;
+                        // console.log("Improved state");
+                    } else {
+                        // console.log("skipped worse state");
+                    }
+                }
+            }
+            states.push(...newStates);
+            processed.push(nextState);
         }
 
         return maxRelease;
@@ -151,7 +201,12 @@ class Network {
                 const newValveRates = new Map(state.valveRates);
                 newValveRates.set(newPos, 0);
                 const newTime = state.time + 1 + state.adjMatrix[posIndex][i];
-                const newAccumulatedRelease = state.accumulatedRelease + (30 - newTime) * state.valveRates.get(newPos)!;
+                let newAccumulatedRelease: number;
+                if (newTime > 30) {
+                    newAccumulatedRelease = state.accumulatedRelease;
+                } else {
+                    newAccumulatedRelease = state.accumulatedRelease + (30 - newTime) * state.valveRates.get(newPos)!;
+                }
                 const firstReleaseState = new SearchState(newPos, state.adjMatrix, newValveRates, newTime, newAccumulatedRelease);
                 res.push(firstReleaseState);
             }
@@ -165,7 +220,32 @@ class Network {
 
 class SearchState {
 
+    heuristic: number
+
+    // constructor(public pos: string, public adjMatrix: Array<Array<number>>, public valveRates: Map<string, number>, public time: number, public accumulatedRelease: number) {
+    //     // const avgValue = Array.from(valveRates.values()).reduce((acc, curr) => acc + curr, 0) / valveRates.size;
+    //     const bestRate = Math.max(...valveRates.values());
+    //     const avgDist = (2 / 3) * adjMatrix.map(r => r.reduce((acc, curr) => acc + curr, 0)).reduce((acc, curr) => acc + curr, 0) / (adjMatrix.length * adjMatrix[0].length);
+    //     const remainingTime = 30 - time;
+    //     const mult = Math.ceil(remainingTime / avgDist);
+    //     this.heuristic = this.accumulatedRelease + (30 - time - avgDist) * bestRate * mult;
+    //     // this.heuristic = this.accumulatedRelease + (29 - time) * Math.min(...valveRates.values());
+    // }
+
     constructor(public pos: string, public adjMatrix: Array<Array<number>>, public valveRates: Map<string, number>, public time: number, public accumulatedRelease: number) {
+        // const avgValue = Array.from(valveRates.values()).reduce((acc, curr) => acc + curr, 0) / valveRates.size;
+        const bestRates = Array.from(valveRates.values()).toSorted((a, b) => b - a);
+        // console.log("  best rates: ", bestRates);
+        let remainingTime = 30 - time;
+        // console.log("  time / remaining time: ", time, remainingTime);
+        let expectedAdditionalRelease = 0;
+        for (remainingTime -= 2; remainingTime > 0; remainingTime -= 2) {
+            const nextRate = bestRates.shift();
+            expectedAdditionalRelease += nextRate! * remainingTime;
+        }
+        // console.log("   g: ", expectedAdditionalRelease);
+        this.heuristic = this.accumulatedRelease + expectedAdditionalRelease;
+        // this.heuristic = this.accumulatedRelease + (29 - time) * Math.min(...valveRates.values());
     }
 }
 
